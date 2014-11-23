814895ba7b34719434ece9e6bed1dba84a94b370
Julian Weiland <developer@julianweiland.de>
2014-11-23 23:31:54 +0100

    Initial Commit

---
+++ Challenge1_Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Challenge1_Postcard
+//
+//  Created by Julian Weiland on 23.11.14.
+//  Copyright (c) 2014 Julian Weiland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
