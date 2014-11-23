f367c174e66b42b598e1ff1894d9393574168d1a
Julian Weiland <developer@julianweiland.de>
2014-11-24 00:14:48 +0100

    done

--- Challenge1_Postcard/ViewController.swift
+++ Challenge1_Postcard/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtName: UITextField!
+    @IBOutlet var txtMessage: UITextField!
+    @IBOutlet var btnSend: UIButton!
+    @IBOutlet var lblResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        btnSend.setTitle("Sending…", forState: UIControlState.Highlighted)
+        lblResult.hidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendButtonPressed(sender: UIButton) {
 
+        if (txtName.hasText()){
+            lblResult.text = txtName.text
+        } else {
+            lblResult.text = "Unknown"
+        }
+        
+        lblResult.text! += " : " + txtMessage.text!
+        txtMessage.resignFirstResponder()
+        btnSend.setTitle("Mail sent", forState: UIControlState.Normal)
+        lblResult.textColor = UIColor.blueColor()
+        lblResult.hidden = false
+        txtName.enabled = false
+        txtMessage.text = ""
+        
+        
+    }
+    
+    
 }
 
